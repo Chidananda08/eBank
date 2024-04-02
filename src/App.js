0c3755ea1eb63a0aa99e86861bc3988f2ac3324a
chidanandaparida95@gmail.com <nxtwave@ip-10-2-0-63.ap-south-1.compute.internal>
2024-04-02 11:13:38 +0000

    ebank

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/ebank/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
